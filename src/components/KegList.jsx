c6390e05f416797f4d1244ee861d4bceb8c220a4
Lisa Jensen <leese_38@yahoo.com>
2019-09-06 10:20:31 -0700

    add base component files

---
+++ src/components/KegList.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import KegList from './KegList';
+import Header from './Header';
+import { Switch, Route } from 'react-router-dom';
+import NewKegForm from './NewKegForm';
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Switch>
+                <Route exact path='/' component={KegList} />
+                <Route path='/newkeg' component={NewKegForm} />
+            </Switch>
+            <KegList />
+        </div>
+    );
+}
+
+export default App;
